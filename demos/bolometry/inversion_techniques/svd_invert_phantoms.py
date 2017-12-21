61d4e4f705290b6d414413a0957ef916ee7ac56a
mattngc <matthewncarr@gmail.com>
2017-12-21 11:27:46 +0000

    Adding SVD as inversion method. General tidy up of some other routines and imports.

--- demos/bolometry/inversion_techniques/svd_invert_phantoms.py
+++ demos/bolometry/inversion_techniques/svd_invert_phantoms.py
@@ -1,27 +1,22 @@
 
-import os
 import numpy as np
+import scipy
 import matplotlib.pyplot as plt
 
-from cherab.tools.inversions import invert_regularised_nnls
+from cherab.tools.inversions import invert_svd
 from cherab.tools.observers.inversion_grid import EmissivityGrid
 from cherab.tools.observers.bolometry import assemble_weight_matrix
-from cherab.aug.bolometry import load_emissivity_phantom, load_standard_inversion_grid, load_default_bolometer_config,\
-    load_blb_config_file, load_blb_result_file
+from cherab.aug.bolometry import load_emissivity_phantom, load_standard_inversion_grid, load_default_bolometer_config
 
 
-# 'FVC1_A_CH1', 'FVC2_E_CH17', 'FLX_A_CH1', 'FHS_A_CH1'
-EXCLUDED_CHANNELS = ['FDC_A_CH1', 'FDC_G_CH28', 'FHC1_A_CH1', 'FHC1_A_CH2', 'FHC1_A_CH3']
+EXCLUDED_CHANNELS = ['FVC1_A_CH1', 'FVC2_E_CH17', 'FLX_A_CH1', 'FDC_A_CH1', 'FDC_G_CH28', 'FHS_A_CH1',
+                     'FHC1_A_CH1', 'FHC1_A_CH2', 'FHC1_A_CH3']
 
-grid = load_standard_inversion_grid()
 
-# AUG_BLB_CASE_DIRECTORY = os.path.expanduser("~/CCFE/mst1/aug_bolometry/BLB.33280_2")
-# active_cameras = load_blb_config_file(os.path.join(AUG_BLB_CASE_DIRECTORY, "config"))
-# result_blb33280_raw = load_blb_result_file(os.path.join(AUG_BLB_CASE_DIRECTORY, "resultef2d.BLB.33280_4.100000"), grid)
-# emiss77 = EmissivityGrid(grid, result_blb33280_raw.case_id, result_blb33280_raw.description, emissivities=np.clip(result_blb33280_raw.emissivities, 0, None))
 emiss77 = load_emissivity_phantom('AUG_emission_phantom_077')
 emiss = emiss77.emissivities
 
+grid = load_standard_inversion_grid()
 
 fhc = load_default_bolometer_config('FHC', inversion_grid=grid)
 flh = load_default_bolometer_config('FLH', inversion_grid=grid)
@@ -51,13 +46,14 @@ obs_power = np.dot(vol_weight_matrix, emiss77.emissivities)
 print("fhc13_obs_power", fhc13_obs_power)
 print("obs_power", obs_power[9])
 
-inverted_emiss_vector, rnorm = invert_regularised_nnls(vol_weight_matrix, obs_power, alpha=0.0000000001)
+inverted_emiss_vector = invert_svd(vol_weight_matrix, obs_power)
 inverted_emiss = EmissivityGrid(grid, case_id='Phantom 77 - LOS method', emissivities=inverted_emiss_vector)
 
 fhc13_inverted_power = 0
 for i in range(len(test_fhc13)):
     fhc13_inverted_power += fhc13_real[i] * inverted_emiss_vector[i]
 print("fhc13_inverted_power", fhc13_inverted_power)
+
 
 plt.ion()
 emiss77.plot()
@@ -69,4 +65,4 @@ plt.axis('equal')
 
 print('Phantom total power', emiss77.total_radiated_power())
 print('Inversion total power', inverted_emiss.total_radiated_power())
-print('rnorm', rnorm)
+
