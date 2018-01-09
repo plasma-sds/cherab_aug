cce08166b51fe2b459074fe8661a750343727066
mattngc <matthewncarr@gmail.com>
2018-01-09 15:42:19 +0000

    Updates to example script of SVD bolometer inversion.

--- demos/bolometry/inversion_techniques/svd_invert_phantoms.py
+++ demos/bolometry/inversion_techniques/svd_invert_phantoms.py
@@ -13,8 +13,8 @@ from cherab.aug.bolometry import load_emissivity_phantom, load_standard_inversio
 EXCLUDED_CHANNELS = ['FDC_A_CH1', 'FDC_G_CH28', 'FHC1_A_CH1', 'FHC1_A_CH2', 'FHC1_A_CH3']
 
 
-emiss77 = load_emissivity_phantom('AUG_emission_phantom_077')
-emiss = emiss77.emissivities
+phantom_emiss = load_emissivity_phantom('AUG_emission_phantom_077')
+emiss = phantom_emiss.emissivities
 
 grid = load_standard_inversion_grid()
 
@@ -29,40 +29,25 @@ flx = load_default_bolometer_config('FLX', inversion_grid=grid)
 detector_keys, los_weight_matrix, vol_weight_matrix = assemble_weight_matrix([fhc, flh, fhs, fvc, fdc, flx],
                                                                              excluded_detectors=EXCLUDED_CHANNELS)
 
-print("vol_weight_matrix shape", vol_weight_matrix.shape)
-fhc13_real = fhc[detector_keys[9]]._volume_power_sensitivity.sensitivity
-test_fhc13 = vol_weight_matrix[9, :]
-
-fhc13_obs_power = 0
-for i in range(len(test_fhc13)):
-    if fhc13_real[i] != test_fhc13[i]:
-        print('Warning {} != {}'.format(fhc13_real[i], test_fhc13[i]))
-
-    fhc13_obs_power += fhc13_real[i] * emiss[i]
-
-
-obs_power = np.dot(vol_weight_matrix, emiss77.emissivities)
-
-print("fhc13_obs_power", fhc13_obs_power)
-print("obs_power", obs_power[9])
-
-inverted_emiss_vector = invert_svd(vol_weight_matrix, obs_power)
-inverted_emiss = EmissivityGrid(grid, case_id='Phantom 77 - LOS method', emissivities=inverted_emiss_vector)
-
-fhc13_inverted_power = 0
-for i in range(len(test_fhc13)):
-    fhc13_inverted_power += fhc13_real[i] * inverted_emiss_vector[i]
-print("fhc13_inverted_power", fhc13_inverted_power)
-
+obs_power = np.dot(vol_weight_matrix, phantom_emiss.emissivities)
 
 plt.ion()
-emiss77.plot()
+phantom_emiss.plot()
 plt.axis('equal')
+print('Phantom total power - {:.4G}MW'.format(phantom_emiss.total_radiated_power()/1E6))
 
+
+inverted_emiss_vector = invert_svd(los_weight_matrix, obs_power)
+inverted_emiss = EmissivityGrid(grid, case_id='Phantom 77 - LOS method', emissivities=inverted_emiss_vector)
 inverted_emiss.plot()
 plt.axis('equal')
+print('SVD LOS Inversion total power - {:.4G}MW'.format(inverted_emiss.total_radiated_power()/1E6))
+print('SVD LOS correlation - {:.3G}'.format(np.corrcoef(phantom_emiss.emissivities, inverted_emiss.emissivities)[0][1]))
 
 
-print('Phantom total power', emiss77.total_radiated_power())
-print('Inversion total power', inverted_emiss.total_radiated_power())
-
+inverted_emiss_vector = invert_svd(vol_weight_matrix, obs_power)
+inverted_emiss = EmissivityGrid(grid, case_id='Phantom 77 - Volume method', emissivities=inverted_emiss_vector)
+inverted_emiss.plot()
+plt.axis('equal')
+print('SVD VOLUME Inversion total power - {:.4G}MW'.format(inverted_emiss.total_radiated_power()/1E6))
+print('SVD VOLUME correlation - {:.3G}'.format(np.corrcoef(phantom_emiss.emissivities, inverted_emiss.emissivities)[0][1]))
